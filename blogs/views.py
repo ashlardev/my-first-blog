627ab4fcc38a1738998cae0e4f144d99f91e5a65
ashlardev <ChristopherFord@pvh.com>
2019-04-25 21:52:46 -0400

    changed views.py

--- blogs/views.py
+++ blogs/views.py
@@ -77,7 +77,7 @@ def add_comment_to_post(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = CommentForm()
-    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+    return render(request, 'blogs/add_comment_to_post.html', {'form': form})
 
 
 @login_required
